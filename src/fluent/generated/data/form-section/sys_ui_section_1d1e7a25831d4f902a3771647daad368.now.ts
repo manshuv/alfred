20e1a2a1439ba4ec4c8e1c0b51a60d9d108ffe40
Manshu V <manshuv@gmail.com>
2026-06-17 11:04:08 -0700

    new build

---
+++ src/fluent/generated/data/form-section/sys_ui_section_1d1e7a25831d4f902a3771647daad368.now.ts
@@ -0,0 +1,14 @@
+import { Record } from '@servicenow/sdk/core'
+
+Record({
+    $id: Now.ID['151e7a25831d4f902a3771647daad3b1'],
+    table: 'sys_ui_section',
+    data: {
+        header: false,
+        name: 'x_snc_alfred_time_entries',
+        sys_domain: 'global',
+        sys_domain_path: '/',
+        title: true,
+        view: 'NULL',
+    },
+})
---
+++ src/fluent/generated/other/sys-ui-element/sys_ui_section_1d1e7a25831d4f902a3771647daad368.now.ts
@@ -0,0 +1,77 @@
+import { Record } from '@servicenow/sdk/core'
+
+Record({
+    $id: Now.ID['d51e7a25831d4f902a3771647daad3b8'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'activity_description',
+        position: 0,
+        sys_ui_section: '151e7a25831d4f902a3771647daad3b1',
+    },
+})
+Record({
+    $id: Now.ID['991eba25831d4f902a3771647daad304'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'duration_hours',
+        position: 1,
+        sys_ui_section: '151e7a25831d4f902a3771647daad3b1',
+    },
+})
+Record({
+    $id: Now.ID['551eba25831d4f902a3771647daad305'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'notes',
+        position: 2,
+        sys_ui_section: '151e7a25831d4f902a3771647daad3b1',
+    },
+})
+Record({
+    $id: Now.ID['111eba25831d4f902a3771647daad306'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.begin_split',
+        position: 3,
+        sys_ui_section: '151e7a25831d4f902a3771647daad3b1',
+        type: '.begin_split',
+    },
+})
+Record({
+    $id: Now.ID['d91eba25831d4f902a3771647daad306'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'category',
+        position: 4,
+        sys_ui_section: '151e7a25831d4f902a3771647daad3b1',
+    },
+})
+Record({
+    $id: Now.ID['951eba25831d4f902a3771647daad307'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.split',
+        position: 5,
+        sys_ui_section: '151e7a25831d4f902a3771647daad3b1',
+        type: '.split',
+    },
+})
+Record({
+    $id: Now.ID['511eba25831d4f902a3771647daad308'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'date',
+        position: 6,
+        sys_ui_section: '151e7a25831d4f902a3771647daad3b1',
+    },
+})
+Record({
+    $id: Now.ID['1d1eba25831d4f902a3771647daad308'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.end_split',
+        position: 7,
+        sys_ui_section: '151e7a25831d4f902a3771647daad3b1',
+        type: '.end_split',
+    },
+})
